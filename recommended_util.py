e7ac56c1470725165964fa5d4fc8593a37326c37
ARK Studio <developer@arkstud.io>
2019-11-03 23:04:47 -0600

    Add randomly set of items from dataset and comments

--- recommended_util.py
+++ recommended_util.py
@@ -1,22 +1,38 @@
-# ========================================================================================================
-#                                          PENDIENTE DE DOCUMENTAR  
-# ========================================================================================================
+# Proyecto de inteligencia artificial: 
+# "Sistema de recomendación"
+# ====================================
+# Elaborado por: 
+# @Ulisies 
+# @Juan Carlos Herrera Mercado 
+# @
+# @ 
+#=====================================
+# Laas referencias son citadas al final
+# de este archivo. 
+#
 
 # import statements
 import pandas as pd
 import pickle
+import random
+
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-
+# Abrimos el modelo creado con anterioridad y lo cargamos en memoria para trabajar con el 
 with open('finalized_model.sav', 'rb') as fid:
     model = pickle.load(fid)
 
-
+# Tomamos el data set original 
 paintings_ds = pd.read_csv("data/clear_data_set.csv")
 
+
+# ========================================================================================================
+#                                           Limpieza de datos 
+# ========================================================================================================
+# Limpiamos el dat set para eliminar los registros  con campos vacios 
 paintings_ds = paintings_ds.dropna(how='any')
 
 # Limpiamos los datos de caracteres no estndar. 
@@ -25,31 +41,32 @@ paintings_ds['title'] = paintings_ds['title'].replace({"-": " "}, regex=True)
 paintings_ds['artist'] = paintings_ds['artist'].replace({"-": " "}, regex=True)
 paintings_ds['styleTipo'] = paintings_ds['styleTipo'].replace({"-": " "}, regex=True)
 
+# Estandarizamos la información en un solo campo
 comb_frame_all_fields = paintings_ds.artist.str.cat(" "+paintings_ds.date.astype(str).str.cat(" "+paintings_ds.genre.astype(str).str.cat(" "+paintings_ds.styleTipo.astype(str).str.cat(" "+paintings_ds.title))))
 
+# Eliminamos caracteres que no son números y letras 
 comb_frame_all_fields = comb_frame_all_fields.replace({"[^A-Za-z0-9 ]+": ""}, regex=True)
 
+# Vectorizamos la información 
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(comb_frame_all_fields)
     
-
-
-
-paintings_ds = pd.read_csv("data/clear_data_set.csv")
-
-
-
-paintings_ds['vectorOfData'] = paintings_ds.artist.str.cat(" "+paintings_ds.date.astype(str).str.cat(" "+paintings_ds.genre.astype(str).str.cat(" "+paintings_ds.styleTipo.astype(str).str.cat(" "+paintings_ds.title))))
-
-paintings_ds['ClusterPrediction'] = ""
-
 
 def cluster_predict(str_input):
     Y = vectorizer.transform(list(str_input))
     prediction = model.predict(Y)
     return prediction
 
-paintings_ds['ClusterPrediction']=paintings_ds.apply(lambda x: cluster_predict(paintings_ds['vectorOfData']), axis=0)
+
+# No sense 
+#paintings_ds = pd.read_csv("data/clear_data_set.csv")
+
+paintings_ds['vectorOfData'] = paintings_ds.artist.str.cat(" "+paintings_ds.date.astype(str).str.cat(" "+paintings_ds.genre.astype(str).str.cat(" "+paintings_ds.styleTipo.astype(str).str.cat(" "+paintings_ds.title))))
+
+paintings_ds['ClusterPrediction'] = ""
+
+paintings_ds['ClusterPrediction'] = paintings_ds.apply(lambda x: cluster_predict(paintings_ds['vectorOfData']), axis=0)
+
 
 
 def recommend_util(str_input):
@@ -58,15 +75,34 @@ def recommend_util(str_input):
     temp_ds = paintings_ds.loc[paintings_ds['title'] == str_input]
 
     temp_ds['InputString'] = temp_ds.artist.str.cat(" "+temp_ds.date.astype(str).str.cat(" "+temp_ds.genre.astype(str).str.cat(" "+temp_ds.styleTipo.astype(str).str.cat(" "+temp_ds.title))))
+    
     str_input = list(temp_ds['InputString'])
     
     prediction_inp = cluster_predict(str_input)
-    prediction_inp = int(prediction_inp)
+    print("here - flag:")
+    print(prediction_inp)
     
+    prediction_inp = int(prediction_inp)
+
+    # Based on the above prediction 10 random courses are recommended from the whole data-frame
+    # Recommendation Logic is kept super-simple for current implementation.    
     temp_ds = paintings_ds.loc[paintings_ds['ClusterPrediction'] == prediction_inp]
+
     temp_ds = temp_ds.sample(10)
     
     return list(temp_ds['title'])
+
+
+
+# ========================================================================================================
+#                                   Método que pidío frontend 
+# ========================================================================================================
+def recommendation_randomly_list(numbers): 
+    random_subset = paintings_ds.sample(n=numbers)
+    print(random_subset.head())
+    return random_subset
+
+
 
 
 
@@ -78,6 +114,8 @@ def recommend_util(str_input):
 
 if __name__ == '__main__':
     queries = ['Uriel', 'Vir Heroicus Sublimis']
+
+    recommendation_randomly_list(10)
 
     for query in queries:
         res = recommend_util(query)
